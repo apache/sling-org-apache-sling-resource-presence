8b703891b7a9daffe3ed93a50a829247df07c714
Oliver Lietz <olli@apache.org>
2018-02-02 22:02:07 +0100

    use Testing PaxExam 1.0.0

--- src/test/java/org/apache/sling/resource/presence/ResourcePresenterTestSupport.java
+++ src/test/java/org/apache/sling/resource/presence/ResourcePresenterTestSupport.java
@@ -20,13 +20,11 @@ package org.apache.sling.resource.presence;
 
 import org.apache.sling.testing.paxexam.TestSupport;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.util.PathUtils;
 
-import static org.apache.sling.testing.paxexam.SlingOptions.slingLaunchpadOakTar;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingQuickstartOakTar;
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 import static org.ops4j.pax.exam.cm.ConfigurationAdminOptions.factoryConfiguration;
 
 public abstract class ResourcePresenterTestSupport extends TestSupport {
@@ -36,7 +34,7 @@ public abstract class ResourcePresenterTestSupport extends TestSupport {
     protected Option baseConfiguration() {
         return composite(
             super.baseConfiguration(),
-            launchpad(),
+            slingQuickstart(),
             // Sling Resource Presenter
             testBundle("bundle.filename"),
             factoryConfiguration("org.apache.sling.serviceusermapping.impl.ServiceUserMapperImpl.amended")
@@ -44,26 +42,14 @@ public abstract class ResourcePresenterTestSupport extends TestSupport {
                 .asOption(),
             // testing
             mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
-            junitBundles(),
-            logging()
+            junitBundles()
         );
     }
 
-    protected Option launchpad() {
+    protected Option slingQuickstart() {
         final int httpPort = findFreePort();
         final String workingDirectory = workingDirectory();
-        return slingLaunchpadOakTar(workingDirectory, httpPort);
-    }
-
-    protected Option logging() {
-        final String filename = String.format("file:%s/src/test/resources/logback.xml", PathUtils.getBaseDir());
-        return composite(
-            systemProperty("logback.configurationFile").value(filename),
-            mavenBundle().groupId("org.slf4j").artifactId("slf4j-api").version("1.7.21"),
-            mavenBundle().groupId("org.slf4j").artifactId("jcl-over-slf4j").version("1.7.21"),
-            mavenBundle().groupId("ch.qos.logback").artifactId("logback-core").version("1.1.7"),
-            mavenBundle().groupId("ch.qos.logback").artifactId("logback-classic").version("1.1.7")
-        );
+        return slingQuickstartOakTar(workingDirectory, httpPort);
     }
 
 }
